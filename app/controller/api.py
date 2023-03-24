1913bb486139472313a11cf11f3e680606c779e9
3shin44 <3shin44@gmail.com>
2023-03-25 00:00:21 +0800

    add csv function

--- app/controller/api.py
+++ app/controller/api.py
@@ -7,52 +7,27 @@ CORS(api)
 # set response mimetype
 from flask import Response
 
+from app.service.service import *
+
 @api.route('/')
 def catch_all():
     return render_template('index.html')
-
-@api.route('/hello')
-def hello():
-    return 'Hello, World!'
 
 @api.route('/version')
 def version():
     # define response content-type
     return Response('v1.0', mimetype='text/plain')
 
-@api.route('/add', methods=['POST'])
-def add():
-    data = request.get_json()
-    a = data.get('a')
-    b = data.get('b')
-    return f'The sum of {a} and {b} is {a+b}.'
-
-@api.route('/dbtest')
-def dbtest():
-    from app.service.service import test_query_myTable
-    return test_query_myTable()
-
-@api.route('/queryByName', methods=['POST'])
-def queryByName():
-    data = request.get_json()
-    name = data.get('name')
-    from app.service.service import query_myTable
-    return query_myTable(name)
-
 @api.route('/startRecord', methods=['get'])
 def startRecord():
-    from app.service.service import serviceStartRecord
     serviceStartRecord()
     return Response("start success", mimetype='text/plain')
 
 @api.route('/stopRecord', methods=['get'])
 def stopRecord():
-    from app.service.service import serviceStopRecord
     serviceStopRecord()
     return "record stop"
 
 @api.route('/getRecordData', methods=['get'])
 def getRecordData():
-    from app.service.service import serviceGetRecordData
-    
     return serviceGetRecordData()
--- app/service/service.py
+++ app/service/service.py
@@ -1,50 +1,4 @@
-# 這個多次呼叫時會爆掉 stack資料顯示可能為BUG
-# from apscheduler.schedulers.background import BackgroundScheduler
-
-# import the module to create a database engine
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy import text  # import the module to execute SQL statements
-
-import json  # import the module to handle JSON data
-
-url = "mysql+pymysql://azag:root@localhost/AZAG_DB"  # define the database URL
-engine = create_engine(url)  # create a database engine
-metadata = MetaData()
-connection = engine.connect()  # connect to the database
-
-# start session
-def stratSesstion():
-    from sqlalchemy.orm import sessionmaker
-    global engine
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
-
-
-def test_query_myTable():  # define a function to test query all data from MYTABLE
-    sql = text("SELECT * FROM myTable")  # write the SQL statement
-    # execute the SQL statement and get the result set
-    row = connection.execute(sql)
-    results = row.fetchall()  # fetch all rows from the result set
-    response = ""  # initialize an empty string for response
-    for row in results:  # loop through each row in results
-        # append each row as a string to response
-        response = response + str(row)
-    return response  # return the response
-
-
-def query_myTable(name):  # define a function to query data from MYTABLE by name
-    # write the SQL statement with name parameter
-    sql = text(f"SELECT * FROM MYTABLE WHERE name='{name}'")
-    # execute the SQL statement and get the result set
-    row = connection.execute(sql)
-    results = row.fetchall()  # fetch all rows from the result set
-    response = ""  # initialize an empty string for response
-    for row in results:
-        response = response + str(row)
-    return response
-
-
+# get sensor measure data
 def getTemperature():
     import random
     value = round(random.uniform(10, 100), 2)
@@ -55,8 +9,6 @@ execFlag = True
 def insertRecord(flag):
     if(not flag):
        return
-    # table model
-    DATALOGGER_table = Table('DATALOGGER', metadata, autoload_with=engine)
     # fetch data from sensor
     from datetime import datetime
     # get current date and time
@@ -64,11 +16,6 @@ def insertRecord(flag):
     time_mark = now.strftime("%Y-%m-%d %H:%M:%S")
     value = getTemperature()
     print(f"insert data: Date:{time_mark}, Value: {value}")
-    # create insert object and define values, commit change
-    ins = DATALOGGER_table.insert().values(DATE=time_mark, TEMP=value)
-    session = stratSesstion()
-    session.execute(ins)
-    session.commit()
     loopSelf()
 
 # 再度執行 insertRecord(寫入紀錄)
@@ -92,16 +39,26 @@ def serviceStopRecord():
 
 # 獲取現存紀錄值
 def serviceGetRecordData():
-    from app.models.mytable import DATALOGGER
-    # query all data from User table and order by ID in descending order 
-    session = stratSesstion()
-    query = session.query(DATALOGGER).order_by(DATALOGGER.DATE.desc()) 
-    results = query.limit(20).all()
-
-    def rowData_to_dict(rowData): # define a function to convert a User object to a dictionary
-        return {"ID": rowData.ID, "DATE": str(rowData.DATE), "TEMP": rowData.TEMP} # return a dictionary with the attributes of the user
-
-    json_data = [rowData_to_dict(d) for d in results]
+    
     return json_data
 
+import csv
+def writeToCSV():
+    
+    with open('DataLogger', 'a', newline='') as file:
+        writer = csv.writer(file)
+        from datetime import datetime
+        now = datetime.now()
+        time_mark = now.strftime("%Y-%m-%d %H:%M:%S")
+        value = getTemperature()
+        print(f"insert data: Date:{time_mark}, Value: {value}")
+        writer.writerow([time_mark, value])
 
+# check DataLogger.csv is exist
+def checkCSVFile():
+    from os.path import exists
+    file_exists = exists("DataLogger.csv")
+    if(not file_exists):
+        print("create file")
+        open("DataLogger.csv", "x").close()
+checkCSVFile()
