b81d53c67f19066d9f1254f37756cfede4a95b76
3shin44 <3shin44@gmail.com>
2023-05-28 23:17:35 +0800

    add start cmd

--- app/service/service.py
+++ app/service/service.py
@@ -1,7 +1,7 @@
 # get sensor measure data
+from temp_read_1_sensor import measureTemp
 def getTemperature():
-    import random
-    value = round(random.uniform(10, 100), 2)
+    value = measureTemp()
     return value
 
 import threading
@@ -44,15 +44,13 @@ def serviceGetRecordData():
 
 import csv
 def writeToCSV():
-    
-    with open('DataLogger', 'a', newline='') as file:
+    with open('DataLogger.csv', 'a', newline='') as file:
         writer = csv.writer(file)
         from datetime import datetime
         now = datetime.now()
         time_mark = now.strftime("%Y-%m-%d %H:%M:%S")
         value = getTemperature()
-        print(f"insert data: Date:{time_mark}, Value: {value}")
-        writer.writerow([time_mark, value])
+        writer.writerow(['DATE', time_mark, 'TEMP', value])
 
 # check DataLogger.csv is exist
 def checkCSVFile():
@@ -62,3 +60,18 @@ def checkCSVFile():
         print("create file")
         open("DataLogger.csv", "x").close()
 checkCSVFile()
+
+
+def csv_to_json():
+    jsonArray = []
+    #read csv file
+    with open('DataLogger.csv', encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+        #convert each csv row into python dict
+        for row in csvReader: 
+            print('herr',row)
+            print('herr',row[])
+            #add this python dict to json array
+            jsonArray.append(row)
+    return jsonArray
